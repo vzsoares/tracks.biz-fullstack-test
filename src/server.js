faeb58aed355445c927ae0383b9aae4c4e3974bf
vzsoares <vinicius.zenha@gmail.com>
2025-09-07 22:56:01 -0300

    feat: swagger docs

--- src/server.js
+++ src/server.js
@@ -1,8 +1,32 @@
 import fastify from "fastify";
+import swagger from "@fastify/swagger";
+import swaggerUi from "@fastify/swagger-ui";
 import artists from "./api/artists.js";
 import playlists from "./api/playlists.js";
 
 const app = fastify({ logger: true });
+
+app.register(swagger, {
+	swagger: {
+		info: {
+			title: "Tracks.biz API",
+			description: "API for tracks.biz",
+			version: "0.1.0",
+		},
+		host: "localhost:3000",
+		schemes: ["http"],
+		consumes: ["application/json"],
+		produces: ["application/json"],
+	},
+});
+
+app.register(swaggerUi, {
+	routePrefix: "/docs",
+	uiConfig: {
+		docExpansion: "full",
+		deepLinking: false,
+	},
+});
 
 app.register(playlists);
 app.register(artists);
@@ -11,7 +35,9 @@ app.get("/health", async () => {
 	return { status: "ok" };
 });
 
-function start() {
+async function start() {
+	await app.ready();
+	app.swagger();
 	try {
 		app.listen({ port: process.env.PORT || 3000 });
 	} catch (err) {
