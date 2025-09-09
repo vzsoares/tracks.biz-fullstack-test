0d38f5bcba28d93f50798f343190375aaca71af1
vzsoares <vinicius.zenha@gmail.com>
2025-09-09 16:51:03 -0300

    chore: mv files

--- ch/10_mv_artist_daily_aggr.sql
+++ ch/10_mv_artist_daily_aggr.sql
@@ -14,9 +14,9 @@ SELECT
     -- toDate returns YYYY-MM-DD
     toDate(added_at) AS event_date,
     artist_id,
-    -- uniqExact will ensure unique track_id values
+    -- uniqExactState will ensure unique track_id values
     -- track_id must be unique because it's only added once
-    uniqExact(track_id) AS unique_tracks_added
+    uniqExactState(track_id) AS unique_tracks_added
 FROM playlist_track_events
 WHERE action = 'add'
 GROUP BY event_date, artist_id;
