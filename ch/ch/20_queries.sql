cc7a423a50d59ea5ac5317edce769430657f615f
vzsoares <vinicius.zenha@gmail.com>
2025-09-09 12:37:27 -0300

    feat: query B

--- ch/ch/20_queries.sql
+++ ch/ch/20_queries.sql
@@ -1,4 +1,4 @@
--- Top 10 artists in last 30 days
+-- A) Top 10 artists in last 30 days
 WITH now() AS ts
 SELECT
   artist_id,
@@ -12,3 +12,15 @@ GROUP BY artist_id
 -- tie breaker: higher average popularity
 ORDER BY uniq_adds DESC, avg_popularity DESC
 LIMIT 10;
+
+-- B) Energy distribution per playlist
+SELECT
+  playlist_id,
+  -- energy distribution
+  quantiles(0.25, 0.5, 0.9)(energy) AS energy_quants,
+  -- most frequent artists
+  topK(5)(artist_id) AS top_artists
+FROM playlist_track_events
+WHERE action = 'add'
+GROUP BY playlist_id
+ORDER BY playlist_id;
